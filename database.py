277f7dd576e7b1f4f69978572c8110b7f86ccee0
Bohdan-Zasiadvovk <zasadvovkb@gmail.com>
2023-10-10 06:10:34 +0300

    Update class for database (add validation)

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import sqlite3
 import json
+from validator import *
+
+val = Validator()
 
 class Database:
     def __init__(self, db_name='bot_dobromir.db'):
@@ -15,17 +18,28 @@ class Database:
         self.cursor.execute('SELECT * FROM users WHERE tg_id = ?', (tg_id,))
         return self.cursor.fetchone()
 
+# If user in database - returns user data
+# if user is not in database (check with tg_id) - call function for create user
     def get_or_create_user(self, tg_id, name, phone, username):
-        user = self.get_user(tg_id)
-        if user:
-            return user
+        # validation input data
+        if val.validate_number(tg_id) & val.validate_text(name) & val.validate_number(phone) & val.validate_text(username):
+            user = self.get_user(tg_id)
+            if user:
+                return user
+            else:
+                self.create_user(tg_id, name, phone, username)
+                return self.get_user(tg_id)
         else:
-            self.create_user(tg_id, name, phone, username)
-            return self.get_user(tg_id)
+            # send error to admin
+            pass
 
     def update_user(self, tg_id, new_username):
-        self.cursor.execute('UPDATE users SET username = ? WHERE tg_id = ?', (new_username, tg_id))
-        self.conn.commit()
+        if val.validate_text(new_username):
+            self.cursor.execute('UPDATE users SET username = ? WHERE tg_id = ?', (new_username, tg_id))
+            self.conn.commit()
+        else:
+            # send error to admin
+            pass
 
     def get_product(self, slug):
         self.cursor.execute('SELECT * FROM products WHERE slug = ?', (slug,))
@@ -36,17 +50,30 @@ class Database:
         return self.cursor.fetchall()
 
     def create_order(self, user_id, order_details, status='new'):
-        self.cursor.execute('INSERT INTO orders (user_id, order_details, status) VALUES (?, ?, ?)',
-                            (user_id, json.dumps(order_details), status))
-        self.conn.commit()
+        if val.validate_dict(order_details):
+            self.cursor.execute('INSERT INTO orders (user_id, order_details, status) VALUES (?, ?, ?)',
+                                (user_id, json.dumps(order_details), status))
+            self.conn.commit()
+        else:
+            # send error to admin
+            pass
 
+# need test
     def get_last_new_by_tg_id(self, tg_id):
-        self.cursor.execute('SELECT * FROM orders WHERE user_id = ? ORDER BY datetime DESC LIMIT 1', (tg_id,))
+        # Get user_id from users
+        self.cursor.execute('SELECT user_id FROM users WHERE tg_id = ?', (tg_id,))
+        user_id = self.cursor.fetchone()
+
+        self.cursor.execute('SELECT * FROM orders WHERE user_id = ? AND status = "new" ORDER BY datetime DESC LIMIT 1', (user_id,))
         return self.cursor.fetchone()
 
     def update_order(self, order_id, order_details):
-        self.cursor.execute('UPDATE orders SET order_details = ? WHERE id = ?', (json.dumps(order_details), order_id))
-        self.conn.commit()
+        if val.validate_dict(order_details):
+            self.cursor.execute('UPDATE orders SET order_details = ? WHERE id = ?', (json.dumps(order_details), order_id))
+            self.conn.commit()
+        else:
+            # send error to admin
+            pass
 
     def set_status_confirmed(self, order_id):
         self.cursor.execute('UPDATE orders SET status = "confirmed" WHERE id = ?', (order_id,))
@@ -60,5 +87,5 @@ class Database:
         self.cursor.execute('UPDATE orders SET status = "deleted" WHERE id = ?', (order_id,))
         self.conn.commit()
 
-    def close_connection(self):
+    def __del__(self):
         self.conn.close()
