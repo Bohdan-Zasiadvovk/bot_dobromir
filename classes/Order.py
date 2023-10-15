4be7c10746f21aa41b1ecbbb317f0e8ff4cfa4b1
Bohdan-Zasiadvovk <zasadvovkb@gmail.com>
2023-10-15 23:14:34 +0300

    Update class for order

--- classes/Order.py
+++ classes/Order.py
@@ -1,20 +1,45 @@
+from database import Database
+import User
+import json
+
 class Order:
-    user = {}
+    DBcursor = None
+    user = None
     order_details = {}
 
-    def __init__(self, user=False, order_details=None, db_object=None):
-        self.user = user
-        if order_details:
-            self.order_details = order_details
-        else:
-            self.order_details = self.get_order()
-            if not self.order_details:
-                self.create_order()
+    def __init__(self, user_tg_id: str, db_cursor: Database, order_details=None):
+        self.user_tg_id = user_tg_id
+        self.DBcursor = db_cursor
+        self.user = User(user_tg_id, db_cursor)
+
+        order_dict = self.db_to_dict()
+
+        self.id = order_dict['id']
+        self.tg_id = order_dict['tg_id']
+        self.order_details = order_dict['order_details']
+        self.datetime = order_dict['datetime']
+        self.status = order_dict['status']
+
+
+    def db_to_dict(self):
+        order_tuple = self.DBcursor.get_last_new_by_tg_id(self.tg_id)
+        if not order_tuple:
+            self.DBcursor.create_order(self.user_tg_id, self.order_details)
+            order_tuple = self.DBcursor.get_last_new_by_tg_id(self.tg_id)
+        order_list = list(order_tuple)
+        order = {
+            'id': order_list[0],
+            'tg_id': order_list[1],
+            'order_details': order_list[2],
+            'datetime': order_list[3],
+            'status': order_list[4],
+        }
+        return order
 
     def create_order(self):
-        self.order_details ={}
+        self.DBcursor.create_order()
 
-    def get_order(self):
+    def get_order_details(self):
         return self.order_details
 
     def update_product(self, product_slug, count):
@@ -48,3 +73,6 @@ class Order:
     def confirmed_order(self):
         self.status = "confirmed"
         return True
+
+    def __del__(self):
+        self.DBcursor.update_order(self.user_tg_id, json.dumps(self.order_details))
