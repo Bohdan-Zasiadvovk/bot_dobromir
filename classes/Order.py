2e79bae29d431c48e5ba0bad8ecca0c5718c00f3
Bohdan-Zasiadvovk <zasadvovkb@gmail.com>
2023-10-17 22:14:09 +0300

    Update class for order (need fix)

--- classes/Order.py
+++ classes/Order.py
@@ -1,21 +1,22 @@
-from database import Database
-import User
+from classes.Database import Database
+from classes.User import User
 import json
 
 class Order:
     DBcursor = None
-    user = None
     order_details = {}
+    id = None
+    user_id = None
 
-    def __init__(self, user_tg_id: str, db_cursor: Database, order_details=None):
-        self.user_tg_id = user_tg_id
+    def __init__(self, tg_id: str, db_cursor: Database, order_details=None):
+        self.tg_id = tg_id
         self.DBcursor = db_cursor
-        self.user = User(user_tg_id, db_cursor)
+        self.user = User(tg_id, db_cursor)
 
         order_dict = self.db_to_dict()
 
         self.id = order_dict['id']
-        self.tg_id = order_dict['tg_id']
+        self.user_id = order_dict['user_id']
         self.order_details = order_dict['order_details']
         self.datetime = order_dict['datetime']
         self.status = order_dict['status']
@@ -24,12 +25,12 @@ class Order:
     def db_to_dict(self):
         order_tuple = self.DBcursor.get_last_new_by_tg_id(self.tg_id)
         if not order_tuple:
-            self.DBcursor.create_order(self.user_tg_id, self.order_details)
+            self.DBcursor.create_order(self.user_id, self.order_details)
             order_tuple = self.DBcursor.get_last_new_by_tg_id(self.tg_id)
         order_list = list(order_tuple)
         order = {
             'id': order_list[0],
-            'tg_id': order_list[1],
+            'user_id': order_list[1],
             'order_details': order_list[2],
             'datetime': order_list[3],
             'status': order_list[4],
@@ -37,7 +38,7 @@ class Order:
         return order
 
     def create_order(self):
-        self.DBcursor.create_order()
+        self.DBcursor.create_order(self.user_id, self.order_details)
 
     def get_order_details(self):
         return self.order_details
@@ -75,4 +76,4 @@ class Order:
         return True
 
     def __del__(self):
-        self.DBcursor.update_order(self.user_tg_id, json.dumps(self.order_details))
+        self.DBcursor.update_order(self.id, json.dumps(self.order_details))
