c09689049246efbef3eac0ae45e893efb01e4866
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2023-10-31 19:54:06 +0200

    Done tests for class Database

--- tests/test_Database.py
+++ tests/test_Database.py
@@ -38,10 +38,25 @@ def test_get_user(test_database):
     assert user[3] == '987654321'
     assert user[4] == 'alice'
 
+def test_get_or_create_user_existing_user(test_database):
+    test_database.create_user('1', 'John Doe', '123456789', 'johndoe')
+    user = test_database.get_or_create_user('1', 'John Doe', '123456789', 'johndoe')
+    assert user is not None
+    assert user[2] == 'John Doe'
+    assert user[3] == '123456789'
+    assert user[4] == 'johndoe'
+
+def test_get_or_create_user_new_user(test_database):
+    user = test_database.get_or_create_user('2', 'Alice', '987654321', 'alice')
+    assert user is not None
+    assert user[2] == 'Alice'
+    assert user[3] == '987654321'
+    assert user[4] == 'alice'
+
 def test_update_user(test_database):
-    test_database.create_user('3', 'Bob', '111111111', 'bob')
-    test_database.update_user('3', new_name='Bob Smith')
-    user = test_database.get_user('3')
+    test_database.create_user('979797', 'Bob', '111111111', 'bob')
+    test_database.update_user('979797', new_name='Bob Smith')
+    user = test_database.get_user('979797')
     assert user is not None
     assert user[2] == 'Bob Smith'
 
@@ -51,7 +66,14 @@ def test_get_product(test_database):
     assert product[1] == 'borsch'
     assert product[2] == 'Борщ'
 
+def test_get_product_list(test_database):
+    product_list = test_database.get_product_list()
+    assert product_list[0] == (1, 'borsch', 'Борщ')
+    assert product_list[1] == (2, 'pea_soup', 'Гороховий суп')
+    assert product_list[2] == (3, 'vermicelli_soup', 'Вермішелевий суп')
+
 def test_create_order(test_database):
+    test_create_user(test_database)
     order_details = {'borsch': 20}
     assert test_database.create_order(1, order_details)
     order = test_database.get_last_new_by_tg_id('123123123')
@@ -61,3 +83,38 @@ def test_create_order(test_database):
     assert order[2] == json.dumps(order_details)  # Check order details
     assert order[4] == 'new'  # Check order status
 
+def test_get_last_new_by_tg_id(test_database):
+    test_create_user(test_database)
+    test_create_order(test_database)
+    last_new_by_tg_id = test_database.get_last_new_by_tg_id('123123123')
+    assert last_new_by_tg_id[4] == 'new'
+
+
+def test_update_order(test_database):
+    test_create_user(test_database)
+    test_create_order(test_database)
+
+
+    updated_order_details = {'borsch': 55}
+    assert test_database.update_order('1', updated_order_details, 'new')
+
+    updated_order = test_database.get_last_new_by_tg_id('123123123')
+    assert updated_order is not None
+    assert json.loads(updated_order[2]) == updated_order_details
+    assert updated_order[4] == 'new'
+
+def test_set_status_confirmed(test_database):
+    test_create_user(test_database)
+    test_create_order(test_database)
+    assert test_database.set_status_confirmed(1) == True
+
+
+def test_set_status_done(test_database):
+    test_create_user(test_database)
+    test_create_order(test_database)
+    assert test_database.set_status_done(1) == True
+
+def test_set_status_deleted(test_database):
+    test_create_user(test_database)
+    test_create_order(test_database)
+    assert test_database.set_status_deleted(1) == True
