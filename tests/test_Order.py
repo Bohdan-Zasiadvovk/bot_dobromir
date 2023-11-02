40d37cb81e085b0fa0fa0a23bbc664c6f6973610
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2023-11-02 21:28:37 +0200

    Add test_db_to_dict (need fix)

--- tests/test_Order.py
+++ tests/test_Order.py
@@ -22,8 +22,20 @@ def test_create_order(test_database):
     assert order.order_details == {}
     assert order.status == 'new'
 
+def test_db_to_dict(test_database):
+    user = User('37373737', test_database, './test_bot_dobromir.db', 'John Doe', '380123456789', 'johndoe')
+    order = Order('37373737', test_database, './test_bot_dobromir.db')
+    print(user.id)
+    order.add_product('borsch')
+    order.set_count('borsch', 20)
+    order_dict = order.db_to_dict()
+    assert isinstance(order_dict, dict)
+    assert order_dict['id'] == 1
+    assert order_dict['user_id'] == 1
+    assert order_dict['order_details'] == {'borsch': 20}
+    assert order_dict['status'] == 'new'
+
 def test_get_order_details(test_database):
-    # order = Order('41414141', test_database, './test_bot_dobromir.db', {'borsch': 20})
     order = Order('41414141', test_database, './test_bot_dobromir.db')
     order.add_product('borsch')
     order.set_count('borsch', 20)
@@ -64,7 +76,7 @@ def test_delete_product(test_database):
     assert order_details is not None
     assert order_details == {}
 
-def test_create_order(test_database):
+def test_confirmed_order(test_database):
     order = Order('41414141', test_database, './test_bot_dobromir.db')
     order.confirmed_order()
     assert order.status == 'confirmed'
